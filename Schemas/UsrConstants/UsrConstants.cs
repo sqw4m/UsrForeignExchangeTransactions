9bd15960a27ce28df18cbae51b5310c8050d55d7
Pachkoriya Georgij <64415668+sqw4m@users.noreply.github.com>
2021-02-18 18:12:44 +0300

    Add files via upload

---
+++ Schemas/UsrConstants/UsrConstants.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Terrasoft.Configuration
+{
+	public static class UsrConstants
+	{
+		public static class MSWordReports
+		{
+			public static string CONTACT_TRANSACTIONS_INFO_ID = "5887e863-9d79-03aa-14fd-187f6c1a3dab";
+		}
+		
+		public static class EmailTemplates
+		{
+			public static string CANCEL_TRANSACTION_TEMPLATE = "0df20a0d-c917-486b-844e-373c3c035f84";
+			public static string DONE_TRANSACTION_TEMPLATE = "2273f577-e180-4d70-b3cf-f08a69483bc8";
+		}
+		
+		public static class TrMessageType
+		{
+			public static string EMAIL = "9a642cd8-d4dc-47a2-867c-026ff43a8438";
+			public static string SMS = "131c4ec2-a6a6-49c2-9755-b9fbcbbd55c7";
+		}
+		
+		public static class CommunicationTypeId
+		{
+			public static string EMAIL = "ee1c85c3-cfcb-df11-9b2a-001d60e938c6";
+			public static string PRIMARY_PHONE = "6a3fb10c-67cc-df11-9b2a-001d60e938c6";
+		}
+	}
+}
